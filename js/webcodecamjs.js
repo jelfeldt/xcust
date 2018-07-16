7e938605f5b65c5f77b5d1d92d9e0eeea8e2f98b
jelfeldt <jeff.elfeldt@xerium.com>
2018-07-16 16:53:34 -0400

    Update webcodecamjs.js

--- js/webcodecamjs.js
+++ js/webcodecamjs.js
@@ -92,7 +92,7 @@ var WebCodeCamJS = function(element) {
 
     function init() {
         var constraints = changeConstraints();
-console.log(constraints);
+console.log('constraints:',constraints);
         try {
             mediaDevices.getUserMedia(constraints).then(cameraSuccess).catch(function(error) {
                 options.cameraError(error);
